f11b5427aac93a0f2c4d230bdbd2e6de1b72ba79
qwinsi <70425035+qwinsi@users.noreply.github.com>
2025-01-15 21:50:48 +0800

    0.3.0-alpha: experimental typst2tex function

--- src/tex2typst.ts
+++ src/tex2typst.ts
@@ -2,8 +2,9 @@
  * This file is the entry point for bundling the .js file for the browser.
  */
 
-import { tex2typst } from './index';
+import { tex2typst, typst2tex } from './index';
 
 if(typeof window !== 'undefined') {
     (window as any).tex2typst = tex2typst;
+    (window as any).typst2tex = typst2tex;
 }
