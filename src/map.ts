84acb4dee9e688f53d7667ff403d928e337b350b
qwinsi <70425035+qwinsi@users.noreply.github.com>
2025-01-15 15:33:08 +0800

    remove redundant code

--- src/map.ts
+++ src/map.ts
@@ -6,7 +6,6 @@ const symbolMap = new Map<string, string>([
     ['ddot', 'dot.double'],
     ['doteq', 'dot(eq)'],
     ['dots', 'dots.h'],
-    ['ldots', 'dots.h'],
     ['vdots', 'dots.v'],
     ['ddots', 'dots.down'],
     ['widehat', 'hat'], // Ideally, the result of \widehat should be longer than \hat. But it is not implemented now.
