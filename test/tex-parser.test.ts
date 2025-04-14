4894bd15ce312d23a557b270a81acccd6383579d
qwinsi <70425035+qwinsi@users.noreply.github.com>
2025-04-14 18:25:03 +0800

    add tex-parser.test.ts

---
+++ test/tex-parser.test.ts
@@ -0,0 +1,57 @@
+import { describe, it, expect } from 'vitest';
+import { tokenize_tex } from '../src/tex-parser';
+import { TexToken, TexTokenType } from '../src/types';
+
+
+describe('typst-tokenizer', () => {
+    it('a + b', function () {
+        const res = tokenize_tex('a + b');
+        expect(res).toEqual([
+            new TexToken(TexTokenType.ELEMENT, 'a'),
+            new TexToken(TexTokenType.SPACE, ' '),
+            new TexToken(TexTokenType.ELEMENT, '+'),
+            new TexToken(TexTokenType.SPACE, ' '),
+            new TexToken(TexTokenType.ELEMENT, 'b'),
+        ]);
+    });
+
+    it('a (x)', function () {
+        const res = tokenize_tex('a (x)');
+        expect(res).toEqual([
+            new TexToken(TexTokenType.ELEMENT, 'a'),
+            new TexToken(TexTokenType.SPACE, ' '),
+            new TexToken(TexTokenType.ELEMENT, '('),
+            new TexToken(TexTokenType.ELEMENT, 'x'),
+            new TexToken(TexTokenType.ELEMENT, ')'),
+        ]);
+    });
+
+    it('f(x)', function () {
+        const res = tokenize_tex('f(x)');
+        expect(res).toEqual([
+            new TexToken(TexTokenType.ELEMENT, 'f'),
+            new TexToken(TexTokenType.ELEMENT, '('),
+            new TexToken(TexTokenType.ELEMENT, 'x'),
+            new TexToken(TexTokenType.ELEMENT, ')'),
+        ]);
+    });
+
+    it('comment', function() {
+        const res = tokenize_tex('a % comment');
+        expect(res).toEqual([
+            new TexToken(TexTokenType.ELEMENT, 'a'),
+            new TexToken(TexTokenType.SPACE, ' '),
+            new TexToken(TexTokenType.COMMENT, ' comment'),
+        ]);
+    });
+
+    it('macro', function() {
+        const res = tokenize_tex('\\sqrt{a}');
+        expect(res).toEqual([
+            new TexToken(TexTokenType.COMMAND, '\\sqrt'),
+            new TexToken(TexTokenType.CONTROL, '{'),
+            new TexToken(TexTokenType.ELEMENT, 'a'),
+            new TexToken(TexTokenType.CONTROL, '}'),
+        ]);
+    })
+});
