d144598c5a77a60559d0e5bcbd9a0a29b49aa593
qwinsi <70425035+qwinsi@users.noreply.github.com>
2025-10-10 23:03:18 +0800

    update `array_intersperse()`

--- src/generic.ts
+++ src/generic.ts
@@ -62,6 +62,7 @@ export function array_split<T extends IEquatable>(array: T[], sep: T): T[][] {
 // e.g. input array=['a', 'b', 'c'], sep = '+'
 // return ['a','+', 'b', '+','c']
 export function array_intersperse<T>(array: T[], sep: T): T[] {
+    /*
     const res: T[] = [];
     for (let i = 0; i < array.length; i++) {
         res.push(array[i]);
@@ -70,4 +71,6 @@ export function array_intersperse<T>(array: T[], sep: T): T[] {
         }
     }
     return res;
+    */
+    return array.flatMap((x, i) => i !== array.length - 1? [x, sep]: [x]);
 }
